a42cc51115fa42db6c9b4c5eb34603c94f970593
Clara-newbie <m.clara.massaro@gmail.com>
2025-02-22 22:59:18 +0100

    svolto esercizio 'useCallback and useMemo'

--- src/useCounter.jsx
+++ src/useCounter.jsx
@@ -1,19 +1,20 @@
+import { useCallback } from "react";
 import { useState } from "react";
 
 export default function useCounter(initialValue = 0) {
   const [counter, setCounter] = useState(initialValue);
 
-  function handleIncrement() {
+  const handleIncrement = useCallback(() => {
     setCounter((c) => c + 1);
-  }
+  }, []);
 
-  function handleDecrement() {
+  const handleDecrement = useCallback(() => {
     setCounter((c) => c - 1);
-  }
+  });
 
-  function handleReset() {
+  const handleReset = useCallback(() => {
     setCounter(initialValue);
-  }
+  });
 
   return {
     counter,
